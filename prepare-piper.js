15c6c5c62cd6466a853d6c057daeb9c2dded0038
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-04-04 23:18:15 -0400

    now with an array of videos

--- prepare-piper.js
+++ prepare-piper.js
@@ -3,11 +3,33 @@ import { readFileSync, writeFileSync } from 'fs';
 import pkg from 'webvtt-parser';
 const { WebVTTParser } = pkg;
 
-const video = 'videos/file.mp4';
-const vtt = 'videos/file.en.vtt';
-const output = 'dataset';
+const videos = ['file1', 'file2'];
 
-processVideo(video, vtt, output);
+let counter = 0;
+let output = '';
+
+for (let video of videos) {
+  processVideo(`videos/${video}.mp4`, `videos/${video}.en.vtt`, 'dataset2');
+}
+
+writeFileSync(`dataset2/metadata.csv`, output);
+
+function processVideo(videoPath, vttPath, outputPath) {
+  const audioPath = `${outputPath}/extracted_audio-${counter}.wav`;
+  videoToWav(videoPath, audioPath);
+
+  const vttData = readFileSync(vttPath, 'utf-8');
+  const parser = new WebVTTParser();
+  const tree = parser.parse(vttData, 'metadata');
+
+  for (let cue of tree.cues) {
+    sliceAudio(audioPath, cue.startTime, cue.endTime, `${outputPath}/wavs/${counter}.wav`);
+    let txt = cue.text.replace(/\n+/g, ' ');
+    txt = txt.replace(/(\&nbsp;)+\s*/g, ' ');
+    output += `wavs/${counter}.wav|${txt}\n`;
+    counter++;
+  }
+}
 
 function videoToWav(videoPath, audioPath) {
   const cmd = `ffmpeg -i "${videoPath}" -acodec pcm_s16le -ar 22050 -ac 1 "${audioPath}"`;
@@ -17,24 +39,4 @@ function videoToWav(videoPath, audioPath) {
 function sliceAudio(audioPath, start, end, segmentPath) {
   const cmd = `ffmpeg -i "${audioPath}" -ss ${start} -to ${end} -c copy "${segmentPath}"`;
   execSync(cmd);
-}
-
-function processVideo(videoPath, vttPath, outputPath) {
-  const audioPath = `${outputPath}/extracted_audio.wav`;
-  videoToWav(videoPath, audioPath);
-
-  const vttData = readFileSync(vttPath, 'utf-8');
-  const parser = new WebVTTParser();
-  const tree = parser.parse(vttData, 'metadata');
-
-  let counter = 0;
-  let output = '';
-  for (let cue of tree.cues) {
-    sliceAudio(audioPath, cue.startTime, cue.endTime, `${outputPath}/wav/${counter}.wav`);
-    let txt = cue.text.replace(/\n+/g, ' ');
-    txt = txt.replace(/(\&nbsp;)+\s*/g, ' ');
-    output += `${counter}|${txt}\n`;
-    counter++;
-  }
-  writeFileSync(`${outputPath}/metadata.csv`, output);
 }
